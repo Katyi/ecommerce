f5797b971d4973482780110f4488693b85a23b46
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-03-02 15:55:08 +0200

    added more codes

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import image_0 from "../src/img/photo_0.png";
+import image_1 from "../src/img/photo_1.png";
+import image_2 from "../src/img/photo_2.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: image_0,
+    title: "SUMMER SALE",
+    desc: `DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ${<br />} FOR NEW ARRIVALS.`,
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: image_1,
+    title: "WINTER SALE",
+    desc: `DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ${<br />} FOR NEW ARRIVALS.`,
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: image_2,
+    title: "POPULAR SALE",
+    desc: `DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ${<br />} FOR NEW ARRIVALS.`,
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: {image_0},
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: {image_0},
+    title: "LOUNGWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: {image_0},
+    title: "LIGHT JACKETS",
+  },
+];
